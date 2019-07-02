ba32770ba86ba915f31ca82eb749f21d38d4bf81
Pedro Henrique <pedro.schleder@gmail.com>
2019-07-01 21:47:32 -0300

    fix connection error

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 let dbURI = 'mongodb://localhost/Loc8r';
 if (process.env.NODE_ENV === 'production') {
-    dbURI = process.env.MONGODB_URI'mongodb://dora:dorah4spass@loc8r-yk5o5.mongodb.net'
+    dbURI = process.env.MONGODB_URI
 }
 mongoose.connect(dbURI, {useNewUrlParser: true});
 
